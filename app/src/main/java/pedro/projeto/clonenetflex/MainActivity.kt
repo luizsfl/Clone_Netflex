c659a0005a55255d3b19d97c8671310a9debdac3
Pedro Luiz <luizsfl@hotmail.com>
2022-02-03 00:14:40 -0300

    layout inicial e inicio do login

---
+++ app/src/main/java/pedro/projeto/clonenetflex/MainActivity.kt
@@ -0,0 +1,27 @@
+package pedro.projeto.clonenetflex
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            AbrirTelaLogin()
+        },3000)
+
+    }
+
+    private fun AbrirTelaLogin(){
+        var intent = Intent(this,FormLogin::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
