614ab4a965556d2ceca2b183cba46a1f53403ef0
Pedro Luiz <luizsfl@hotmail.com>
2022-02-05 18:15:55 -0300

    Conexão com firebase e logar com email e senha

--- app/src/main/java/pedro/projeto/clonenetflix/FormLogin.kt
+++ app/src/main/java/pedro/projeto/clonenetflix/FormLogin.kt
@@ -3,6 +3,10 @@ package pedro.projeto.clonenetflix
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import pedro.projeto.clonenetflex.R
 import pedro.projeto.clonenetflex.databinding.ActivityFormLoginBinding
 import pedro.projeto.clonenetflex.databinding.ActivityMainBinding
@@ -26,6 +30,35 @@ class FormLogin : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.btEntrar.setOnClickListener {
+            val email = binding.editLogin.text.toString()
+            val senha = binding.editSenha.text.toString()
+            var mernsagem_erro = binding.mensageErro
+            if (email.isEmpty() || senha.isEmpty())
+            {
+                mernsagem_erro.setText("Prencha os campos de e-mail e senha")
+            }else{
+                AutenticarUsuário(email,senha,mernsagem_erro)
+            }
 
+        }
+
+
+    }
+    private fun AutenticarUsuário(email:String,senha:String,mernsagem_erro:TextView){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener {
+          if (it.isSuccessful){
+              Toast.makeText(this,"Login efetuado com sucesso",Toast.LENGTH_SHORT).show()
+              IrParaTelaFilmes()
+          }else{
+              mernsagem_erro.setText("Erro ao tentar logar"+it)
+          }
+        }
+    }
+
+    private fun IrParaTelaFilmes(){
+        val intent = Intent(this,ListaFilmes::class.java)
+        startActivity(intent)
+       // finish()
     }
 }
---
+++ app/src/main/java/pedro/projeto/clonenetflix/ListaFilmes.kt
@@ -0,0 +1,12 @@
+package pedro.projeto.clonenetflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pedro.projeto.clonenetflex.R
+
+class ListaFilmes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_filmes)
+    }
+}
